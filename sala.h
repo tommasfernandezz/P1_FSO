38961ed1a6833c38a45f89c24ab64bed77838778
Tomas Fernandez <148056878+tommasfernandezz@users.noreply.github.com>
2026-03-02 20:35:07 +0000

    Update sala.h

--- sala.h
+++ sala.h
@@ -1,1 +1,13 @@
+#ifndef SALA_H
+#define SALA_H
 
+int crea_sala(int capacidad);
+int elimina_sala();
+int reserva_asiento(int id_persona);
+int libera_asiento(int id_asiento);
+int estado_asiento(int id_asiento);
+int asientos_libres();
+int asientos_ocupados();
+int capacidad_sala();
+
+#endif
