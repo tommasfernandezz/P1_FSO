4336648f7fd1ee45ecd633f205892690240a11a4
Tomas Fernandez <148056878+tommasfernandezz@users.noreply.github.com>
2026-03-02 20:37:22 +0000

    Update sala.c

--- sala.c
+++ sala.c
@@ -5,48 +5,46 @@ static int capacidad_actual = -1;
 static int ocupados = 0;
 
 int crea_sala(int capacidad) {
-if (asientos != NULL || capacidad <= 0) {
-return -1;
-}
-asientos = (int*) malloc(capacidad*sizeof(int));
-if (asientos == NULL) {
-return -1;
-}
-capacidad_actual = capacidad;
-ocupados = 0;
-return capacidad_actual;
+  if (asientos != NULL || capacidad <= 0) {
+    return -1;
+  }
+  asientos = (int*) malloc(capacidad*sizeof(int));
+  if (asientos == NULL) {
+    return -1;
+  }
+  capacidad_actual = capacidad;
+  ocupados = 0;
+  return capacidad_actual;
 }
 
 int elimina_sala() {
-if (asientos == NULL) {
-return -1;
-}
-free(asientos);
-asientos = NULL;
-capacidad_actual = -1;
-ocupados = 0;
-return 0;
-
-
+  if (asientos == NULL) {
+    return -1;
+  }
+  free(asientos);
+  asientos = NULL;
+  capacidad_actual = -1;
+  ocupados = 0;
+  return 0;
 }
 
 int reserva_asiento(int id_persona) {
-if (asientos == NULL || id_persona <= 0) {
-return -1;
-}
+  if (asientos == NULL || id_persona <= 0) {
+    return -1;
+  }
 
-if (ocupados >= capacidad_actual) {
-return -1;
-}
+  if (ocupados >= capacidad_actual) {
+    return -1;
+  }
 
-for (int i = 0; i < capacidad_actual; i++) {
-if (asientos[i] == 0) {
-asientos[i] = id_persona;
-ocupados++;
-return i +1;
-}
-}
-return -1;
+  for (int i = 0; i < capacidad_actual; i++) {
+    if (asientos[i] == 0) {
+      asientos[i] = id_persona;
+      ocupados++;
+      return i +1;
+    }
+  }
+  return -1;
 }
 
 int libera_asiento(int id_asiento) {
